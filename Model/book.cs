df18b748cbc52fed5aa2b2cb1a2e765f3d1c6ee4
miljana <miljanacakicmc@gmail.com>
2024-02-07 12:14:51 +0100

    model v2

--- Model/book.cs
+++ Model/book.cs
@@ -7,7 +7,9 @@ public class Book
     public string Title { get; set; } = null!;
     public string Author { get; set; }  = null!;
     public string Language { get; set; } = null!;
-    public string ExternalLink { get; set; } = null!;
+    public string? Description { get; set; }
+    public int? Pages { get; set; }
+    public string? ExternalLink { get; set; }
     public string CoverPath { get; set; } = null!;
     public IEnumerable<string> Genres { get; set; } = new List<string>();
     public Theme? LiteraryTheme { get; set; }
